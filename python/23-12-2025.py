7036d9d9981b4dc6f93962aba54aec23aaf2a8a2
Mat Ol <matolszakowski@gmail.com>
2025-12-23 20:49:24 +0100

    First exercise

---
+++ python/23-12-2025.py
@@ -0,0 +1,21 @@
+"""
+Exercise no. 1
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+"""
+
+def check_k(k, bunch_of_numbers):
+    for number in bunch_of_numbers:
+        for current in range(len(bunch_of_numbers)):
+            if number == bunch_of_numbers[current]:
+                continue
+            if bunch_of_numbers[current] + number == k:
+                return True
+        bunch_of_numbers.pop(0)
+    return False
+
+print(check_k(10, [2, 6, 5, 7]))
+print(check_k(10, [10, 15, 3, 7]), end="\n")
+print(check_k(10, [2, 6, 4, 7]))
