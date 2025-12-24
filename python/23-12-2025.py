5c877e2c3cf4ddd897688a316f28404645290e36
Mat Ol <matolszakowski@gmail.com>
2025-12-24 10:05:40 +0100

    Made O(n) complexity solution

--- python/23-12-2025.py
+++ python/23-12-2025.py
@@ -6,6 +6,8 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 """
 
+
+# O(n^2)
 def check_k(k, bunch_of_numbers):
     for number in bunch_of_numbers:
         for current in range(len(bunch_of_numbers)):
@@ -16,6 +18,24 @@ def check_k(k, bunch_of_numbers):
         bunch_of_numbers.pop(0)
     return False
 
+
+# O(n)
+def check_sum(k, bunch_of_numbers):
+    for number in range(len(bunch_of_numbers)):
+        try:
+            numbers_except_current = bunch_of_numbers[:]
+            del numbers_except_current[number]
+            if k - bunch_of_numbers[number] in numbers_except_current:
+                return True
+        except:
+            continue
+    return False
+
 print(check_k(10, [2, 6, 5, 7]))
-print(check_k(10, [10, 15, 3, 7]), end="\n")
+print(check_k(10, [10, 15, 3, 7]))
 print(check_k(10, [2, 6, 4, 7]))
+
+print(check_sum(10, [2, 6, 5, 7]))
+print(check_sum(10, [10, 15, 3, 7]))
+print(check_sum(10, [2, 6, 4, 7]))
+
